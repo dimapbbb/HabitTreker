6711131c33a047af30bea0af2581d59e67460c99
dimapb <1st.trillionaire.dm@gmail.com>
2024-10-29 21:28:40 +0300

    Настсроена пагинация. Вывод 5 привычек на страницу

--- habit/views.py
+++ habit/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 
 from habit.models import Habit
+from habit.pagination import Pagination
 from habit.serializers import HabitSerializer
 from users.models import User
 
@@ -11,8 +12,14 @@ class HabitViewSet(viewsets.ModelViewSet):
     """
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    pagination_class = Pagination
 
     def perform_create(self, serializer):
         serializer.save(
             user=User.objects.get(id=1)
         )
+
+    def get(self, request):
+        paginated_queryset = self.paginate_queryset(self.queryset)
+        serializer = self.serializer_class(paginated_queryset, many=True)
+        return self.get_paginated_response(serializer.data)
